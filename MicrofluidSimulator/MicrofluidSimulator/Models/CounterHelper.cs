3fb8dfbc8d09d2f9c751d75c43c448ba92bf9652
Moosecj <carlj0908@gmail.com>
2022-02-10 17:28:22 +0100

    JSON reader attempt
    Attempt of reading JSON file after implementing the nwetonsoft json software in the project structure

--- MicrofluidSimulator/MicrofluidSimulator/Models/CounterHelper.cs
+++ MicrofluidSimulator/MicrofluidSimulator/Models/CounterHelper.cs
@@ -4,10 +4,17 @@
     {
         public int incrementer(int num)
         {
+
+            // potentially use namespace to get rid of the complete path to information and other classes from different folders?
             MicrofluidSimulator.SimulatorCode.Information information = new MicrofluidSimulator.SimulatorCode.Information();
             information.Platform_name = "platformNameTest";
             Console.WriteLine(information.Platform_name);
 
+            // attempt of reading json files
+            MicrofluidSimulator.SimulatorCode.Initialize.JSONReader jSONReader = new MicrofluidSimulator.SimulatorCode.Initialize.JSONReader("JSONFiles/platform640.json") ;
+            jSONReader.LoadJson();
+            Console.WriteLine(jSONReader.Json);
+            Console.WriteLine(Environment.CurrentDirectory);
             return num + 22;
         }
     }
--- MicrofluidSimulator/MicrofluidSimulator/SimulatorCode/Initialize/JSONReader.cs
+++ MicrofluidSimulator/MicrofluidSimulator/SimulatorCode/Initialize/JSONReader.cs
@@ -3,8 +3,29 @@
     using Newtonsoft.Json;
     public class JSONReader
     {
+        // attempt of creating json reader
+        string path, json;
+
+        public JSONReader(string path)
+        {
+            this.path = path;
+        }
 
         
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader(path))
+            {
+                json = r.ReadToEnd();
+
+            }
+            
+        }
+
+        public string Json { get => json; set => json = value; }
+        //dynamic information = JObject.Parse("{ 'Name': 'Jon Smith', 'Address': { 'City': 'New York', 'State': 'NY' }, 'Age': 42 }");
+
 
     } 
 }
