92bf5ab58727227bb3005d1e56114a5d8a913c58
Moosecj <carlj0908@gmail.com>
2022-02-14 10:51:03 +0100

    Subscription datatype added

---
+++ MicrofluidSimulator/MicrofluidSimulator/SimulatorCode/DataTypes/Subscriptions.cs
@@ -0,0 +1,19 @@
+﻿namespace MicrofluidSimulator.SimulatorCode.DataTypes
+{
+    public class Subscriptions
+    {
+        int subscriptionType;
+
+        
+
+        public Subscriptions(int subscription)
+        {
+            this.subscriptionType = subscriptionType;
+        }
+
+        public Subscriptions()
+        {
+            this.subscriptionType = -1; //null
+        }
+    }
+}
