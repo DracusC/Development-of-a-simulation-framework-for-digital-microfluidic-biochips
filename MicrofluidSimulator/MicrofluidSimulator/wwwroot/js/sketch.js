697a28f8481d441760385ce8d1c0e9087c4ab9f4
Joel Madsen <joel.madsen10@gmail.com>
2022-02-23 10:00:14 +0100

    Simulator GUI connection work in progress

--- MicrofluidSimulator/MicrofluidSimulator/wwwroot/js/sketch.js
+++ MicrofluidSimulator/MicrofluidSimulator/wwwroot/js/sketch.js
@@ -2,80 +2,82 @@
  * Written by Joel A. V. Madsen
  */
 
+
+// Setup the p5js instance
 window.setp5 = () => {
     new p5(sketch, window.document.getElementById('container'));
     return true;
 };
 
+// Global methods called by c#
+window.get_gui_status = () => {
+    return gui_broker.gui_status;
+};
+window.update_droplets = (droplet_arr) => {
+    gui_broker.gui_status = false;
+
+    var droplets = JSON.parse(droplet_arr);
+    console.log(droplets);
+    gui_broker.droplets = droplets;
+
+
+    droplet_info.old = droplet_info.new;
+    droplet_info.new = droplets;
+
+    return gui_broker;
+}
+window.update_electrodes = (electrodes) => {
+    gui_broker.electrodes = electrodes;
+    return gui_broker;
+}
+window.update_board = (container_string) => {
+    var board = JSON.parse(container_string);
+    gui_broker.droplets = board.droplets;
+    gui_broker.electrodes = board.electrodes;
+}
+
+
+// Declare global variables
 let arr = [];
 let d_info = [];
+let t = "";
+
+// Store simulator board info
+let simulator_droplets = [];
+let simulator_electrodes = [];
+
+
+// gui_broker acts as a connection between the simulator and gui
+let gui_broker = {
+    gui_status: true, // Ready for a new update (true/false)
+    droplets: [],
+    electrodes: []
+};
+let droplet_info = {
+    old: [],
+    new: []
+};
+
+
+
+let simulator_data = {
+    changet: (nt) => {
+        t = nt;
+    }
+};
+
+window.simulator_data = simulator_data;
+window.gui_broker = gui_broker;
  
-let sketch = function(p){
- 
+let sketch = function (p) {
+    
+
     p.setup = function(){ 
         canvas = p.createCanvas(401, 401);
-
+        console.log(gui_broker);
         arr = [];
-
-
-        d_info = [
-            {
-                name: "droplet_1", color: { r: 255, g: 0, b: 0 },
-                pos: [p.createVector(170, 10),
-                p.createVector(170, 10),
-                p.createVector(170, 30),
-                p.createVector(190, 30),
-                p.createVector(190, 30),
-                p.createVector(210, 30),
-                p.createVector(210, 10),
-                p.createVector(190, 10),
-                p.createVector(170, 10),
-                p.createVector(170, 10)],
-                size: { w: 20, h: 20 }
-            },
-            {
-                name: "droplet_2", color: { r: 0, g: 255, b: 0 },
-                pos: [p.createVector(p.width - 171, 10),
-                p.createVector(p.width - 171, 10),
-                p.createVector(p.width - 171, 30),
-                p.createVector(p.width - 191, 30),
-                p.createVector(p.width - 191, 10),
-                p.createVector(p.width - 211, 10),
-                p.createVector(p.width - 211, 30),
-                p.createVector(p.width - 191, 30),
-                p.createVector(p.width - 171, 30),
-                p.createVector(p.width - 171, 10)],
-                size: { w: 20, h: 20 }
-            },
-            {
-                name: "droplet_3", color: { r: 0, g: 255, b: 0 },
-                pos: [p.createVector(170, 110),
-                p.createVector(170, 110),
-                p.createVector(170, 130),
-                p.createVector(190, 130),
-                p.createVector(190, 130),
-                p.createVector(190, 130),
-                p.createVector(190, 130),
-                p.createVector(190, 110),
-                p.createVector(170, 110),
-                p.createVector(170, 110)],
-                size: { w: 20, h: 20 }
-            },
-            {
-                name: "droplet_4", color: { r: 255, g: 0, b: 0 },
-                pos: [p.createVector(p.width - 171, 110),
-                p.createVector(p.width - 171, 110),
-                p.createVector(p.width - 171, 130),
-                p.createVector(p.width - 191, 130),
-                p.createVector(p.width - 211, 130),
-                p.createVector(p.width - 211, 130),
-                p.createVector(p.width - 211, 130),
-                p.createVector(p.width - 191, 130),
-                p.createVector(p.width - 171, 130),
-                p.createVector(p.width - 171, 110)],
-                size: { w: 20, h: 20 }
-            }
-        ];
+        //Testing
+        t = "BEFORE";
     }
 
     let step = 0.02;
@@ -98,6 +100,15 @@ let sketch = function(p){
             p.fill(d_info[i].color.r, d_info[i].color.g, d_info[i].color.b, 127);
             anim_move();
         }
+
+        //console.log(gui_broker.droplets);
+
+        for (let i = 0; i < gui_broker.droplets.length; i++) {
+            let droplet = gui_broker.droplets[i];
+            p.fill(droplet.Color);
+            p.ellipse(droplet.PositionX, droplet.PositionY, droplet.SizeX, droplet.SizeY);
+        }
+
     }
 
     function anim_move() {
@@ -117,4 +128,10 @@ let sketch = function(p){
         p.ellipse(d1x, d1y, 20, 20);
     }
 
+
+    function custom_input() {
+        console.log("INPUT");
+    }
 };
+
+
